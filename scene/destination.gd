e28d9ef8c93c6f33687f3f722b51fe970a201730
Bapt890 <bapt890@gmail.com>
2025-11-08 16:12:25 +0100

    PNJ - Déplacement
    Le PNJ se déplace entre plusieurs destinations. On peut définir certaines comme des arrêts. La vision n'est pas encore faite.

---
+++ scene/destination.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+class_name Destination
+
+# Stops the NPC
+@export var cause_stop: bool = false
+# Stops for how long ?
+@export var stop_duration: int = 0
+# Where do the NPC look ?
+@export_enum("Up Left", "Up Right", "Down Left", "Down Right") var orientation: int
+# Array of linked destinations
+@export var linked_destinations : Array[Destination]
+# Last chosen destination, avoid immediate backtracking
+var last_destination : Destination = null
+
+signal end_job()
+
+# Sets up the timer
+func _ready(): 
+	if cause_stop: $Timer.wait_time = float(stop_duration)
+
+func get_random_destination() -> Destination:
+	# Get a random linked destination
+	var new_dest = linked_destinations[randi_range(0, linked_destinations.size()-1)]
+	# If this new destination is equal to the last one chosen, except if there is only one linked one, redo it
+	while new_dest == last_destination and linked_destinations.size() > 1:
+		new_dest = linked_destinations[randi_range(0, linked_destinations.size()-1)]
+	return new_dest
+
+func set_coming_destination(dest : Destination): last_destination = dest
+
+func start_job(): if $Timer.is_stopped(): $Timer.start()
+
+func _on_timer_timeout(): emit_signal("end_job")
---
+++ scene/npc.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+enum State {IDLE, WALK, INSPECT, CALL, TRAPPED}
+
+const speed = 1000
+# The destination to go to
+@export var target : Destination = null
+var current_destination : Destination = null
+# Objects in FOV
+var sighted_objects : Array
+# Used for State Base Machine
+var current_state = State.IDLE
+# Used to check movement
+var previous_pos : Vector2 = Vector2.ZERO
+var doing_job: bool = false
+
+func _process(delta):
+	match current_state:
+		State.WALK:
+			if position.distance_to(previous_pos) <= 0.01 * delta: 
+				# Stopped moving
+				# Pauses according to reached destination
+				if target.cause_stop: 
+					current_destination = target
+					target = null
+					doing_job = true
+					current_state = State.IDLE
+				# Gives new destination
+				else: 
+					current_destination = target
+					target = target.get_random_destination()
+					go_to()
+			previous_pos = position
+		State.IDLE:
+			if doing_job:
+				doing_job = false
+				current_destination.start_job()
+				await current_destination.end_job
+				target = current_destination.get_random_destination()
+			# If it has a target, goes to it
+			elif target: go_to()
+
+func wait_job():
+	target.start_job()
+	await target.end_job
+	target = target.get_random_destination()
+
+func go_to():
+	var tween = create_tween()
+	tween.tween_property(self, "position", target.position, position.distance_to(target.position) / speed)
+	target.set_coming_destination(current_destination)
+	if current_destination: current_destination.set_coming_destination(null)
+	current_state = State.WALK
+
+#func _physics_process(delta):
+	#var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#velocity = cartesian_to_isometric(direction * 10000 * delta)
+	#move_and_slide()
+	#
+#func cartesian_to_isometric(cartesian):
+	#var screen_pos = Vector2()
+	#screen_pos.x = cartesian.x - cartesian.y
+	#screen_pos.y = cartesian.x / 2 + cartesian.y / 2
+	#return screen_pos
