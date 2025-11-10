d61025f9b7900da8b0a8390a6b32ef93775dc767
razael170 <eleazar.floirat@gmail.com>
2025-11-10 17:25:06 +0100

    menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 
-
 func _on_texture_rect_2_pressed() -> void:
 	$TextureRect2.visible = false
 
@@ -10,9 +9,17 @@ func _on_start_2_pressed() -> void:
 	$TextureRect2.visible = true
 
 
+
 func _on_button_pressed() -> void:
 	pass # Replace with function body.
 
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_Texture_rect3_pressed() -> void:
+	$TextureRect3.visible = false
+
+func _on_controles_pressed() -> void:
+	$TextureRect3.visible = true
