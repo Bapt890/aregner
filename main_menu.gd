792627b7fbb45a2308b482f9063f62d016257168
psychowolf960 <119869367+psychowolf960@users.noreply.github.com>
2025-11-12 16:19:23 +0100

    Main menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,25 +1,25 @@
-extends Node2D
+extends Control
 
+func _slide_in(page):
+	var tween = create_tween()
+	page.position.y = 1080
+	tween.tween_property(page, "position:y", 0, 0.5).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
 
-func _on_texture_rect_2_pressed() -> void:
-	$TextureRect2.visible = false
+func _slide_out(page: Control):
+	var tween = create_tween()
+	tween.tween_property(page, "position:y", 1080, 0.5).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
 
+func _on_credits_pressed() -> void:
+	_slide_in($credits)
 
-func _on_start_2_pressed() -> void:
-	$TextureRect2.visible = true
+func _on_close_credits_pressed() -> void:
+	_slide_out($credits)
 
+func _on_controls_pressed() -> void:
+	_slide_in($controls)
 
-
-func _on_button_pressed() -> void:
-	pass # Replace with function body.
-
+func _on_close_controls_pressed() -> void:
+	_slide_out($controls)
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
-
-
-func _on_Texture_rect3_pressed() -> void:
-	$TextureRect3.visible = false
-
-func _on_controles_pressed() -> void:
-	$TextureRect3.visible = true
